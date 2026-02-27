3935c5eaa3f78f3e3de5512bdd80f8578c681016
EddieO23 <eocampo52@gmail.com>
2026-02-27 06:44:19 -0600

    added react-router-dom. added react-router-bootstrap. replaced a tags with LinkContainer from react-router-bootstrap. added products.js file for mock up data. set up routes for pages

--- proshop-ui/src/main.jsx
+++ proshop-ui/src/main.jsx
@@ -1,12 +1,29 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+
+
 import App from './App.jsx';
+import HomePage from './pages/HomePage.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomePage />} />
+    </Route>,
+  ),
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
