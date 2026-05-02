0e91cedfe8769d41ea5492c0b0a405f98273f7cd
EddieO23 <eocampo52@gmail.com>
2026-05-01 21:59:56 -0500

    started working on implementing the add to cart functinoality. added select option to select qty.

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
 
 // My files
 import Rating from '../components/Rating';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import { addToCart } from '../slices/cartSlice';
 
 const ProductsPage = () => {
   const { id: productId } = useParams();
+
+  const [qty, setQty] = useState(1);
+
+  const addToCartHandler = () => {
+
+  }
+
   const {
     data: product,
     isLoading,
@@ -74,11 +83,33 @@ const ProductsPage = () => {
                   </Row>
                 </ListGroup.Item>
 
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(Number(e.target.value))}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
                     className='btn-block'
                     type='button'
                     disabled={product.countInStock === 0}
+                    onClick= {addToCartHandler}
                   >
                     Add to Cart
                   </Button>
