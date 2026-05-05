55fd49e7bb12a9419e752af406a435748448700c
EddieO23 <eocampo52@gmail.com>
2026-05-05 11:13:38 -0500

    added badge for showing cart items in Header.jsx

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,5 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// My files
+import { updateCart } from '../utils/cartUtils';
+
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
@@ -20,28 +23,11 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-      // Calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0),
-      );
-      // Calculate shipping price (If order is over $100 then free, else $10 shipping)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-      // Calculate tax price (15% tax)
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2)),
-      );
-      // Calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
 
-export const {addToCart} = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
