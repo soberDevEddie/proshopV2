ce01b174eb86cc4f667d008dcc3290d81ff24323
EddieO23 <eocampo52@gmail.com>
2026-05-06 21:14:43 -0500

    started working on backend auth. Added userRoutes and added userController

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+// My files
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+} from '../controllers/userController.js';
+import { get } from 'mongoose';
+const router = express.Router();
+
+router.route('/').post(registerUser).get(getUsers);
+router.post('/logout', logoutUser);
+router.post('/login', authUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 // My files
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import connectDB from './config/db.js';
@@ -20,10 +21,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
