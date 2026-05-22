ea9a798f2353fb813bf6395aecf5368028d7b313
EddieO23 <eocampo52@gmail.com>
2026-05-21 21:30:51 -0500

    can logout user.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,7 +57,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  Post /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('Logout user');
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: 'User logged out successfully' });
 });
 
 // @desc   Get user profile
