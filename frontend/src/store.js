94a6f89e4d2e926269ec7f9404826ea6381997a4
EddieO23 <eocampo52@gmail.com>
2026-06-26 14:11:13 -0500

    successfully set up redux api slice for users

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 // My files
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
+import authSliceReducer from './slices/authSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
