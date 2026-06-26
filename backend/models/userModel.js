a55271043362ce2cf03e84f15664a43f09b0a0fb
EddieO23 <eocampo52@gmail.com>
2026-06-26 13:59:05 -0500

    successfully fixed middleware next function not being able to update user.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,7 +33,7 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
